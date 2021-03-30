0eeaa7d53c4b798f6c7e47efed3fcb314c6eeddf
vccheng2001 <vccheng@andrew.cmu.edu>
2021-03-29 20:59:32 -0700

    3.29 updated preprocessing

--- RNN/src/preprocessing.py
+++ RNN/src/preprocessing.py
@@ -10,17 +10,15 @@ Example: python3 dreams preprocessing.py osa 160
 import glob
 import pandas as pd 
 import os
-import numpy as np
 import csv
-import shutil
 import sys
-
+import shutil
 
 (program, data, apnea_type, timesteps) = sys.argv
-raw_path = f"../{data}/RAW/raw_{apnea_type}/"
-train_path = f"../{data}/TRAIN/train_{apnea_type}/"
-test_path = f"../{data}/TEST/test_{apnea_type}/"
-labels = ["positive/", "negative/"]
+raw_path =      f"../{data}/RAW/raw_{apnea_type}/"
+train_path =    f"../{data}/TRAIN/train_{apnea_type}/"
+test_path =     f"../{data}/TEST/test_{apnea_type}/"
+labels =        ["positive/", "negative/"]
 
 # Preprocesses apnea files 
 def main():
@@ -29,7 +27,7 @@ def main():
         setup_train_data(raw_path, label)
     for label in labels:
         num_files = len(os.listdir(raw_path + label))
-        print(f"Number of {label[:-1]}s: {str(num_files)}")
+        print(f"Parsed {str(num_files)} {label[:-1]} sequences.")
 
 # Sets up directories for train, test data 
 def init_dirs():
@@ -43,45 +41,39 @@ def init_dirs():
 
 # Preprocesses raw data into train data
 def setup_train_data(raw_path,label):
-    # rootdir = raw_path + label + "*"
-    # dirs = glob.glob(rootdir)
-
-    # for d in dirs:
-    #     files = os.listdir(d)
-
     dirs = raw_path + label
     files = os.listdir(dirs) 
     num_files = len(files)
-    num_train = num_files * 0.8
+    num_train = num_files * 0.8 # use 80% for train
 
     # Read each file 
     i = 0
     for file_name in files:
         file_path = f"{dirs}/{file_name}"
-        # Input raw file
-        # print(f"Input:{file_name}")
-
-        # train-test-split 
-        if i < num_train: # train 80%
-            out_file = f"{train_path}{label}{label[:-1]}_{str(i)}.txt"
-        else:             # test 20%
-            out_file = f"{test_path}{label}{label[:-1]}_{str(i)}.txt"
-
+        # print(f"Input:{file_name}") # input
+        path = train_path if i < num_train else test_path
+        out_file = f"{path}{label}{label[:-1]}_{str(i)}.txt"
+        
         try:
             # Read raw file
             df = pd.read_csv(file_path, skip_blank_lines=True, header=None, sep="\n")
             df.dropna(axis=0,inplace=True)
+            # Keep only <timesteps> rows
             df = df.head(int(timesteps))
-            # Output 
-            if not df.empty and df.shape[0] == int(timesteps):
-                # print("Output:" , out_file)
-                df.to_csv(out_file, index=False, header=None,sep="\n",float_format='%.4f')
-
-        except Exception as err:
-            print(err)
+            # print("Output:" , out_file) # output
+            df.to_csv(out_file, index=False, header=None, sep="\n", float_format='%.4f')
+        except Exception as e:
+            print(f"Error: {e}")
             os.remove(file_path)
             continue
         i+=1
+
+
+# Makes directory 
+def make_dir(path):
+    if not os.path.isdir(path):
+        print("Making directory.... " + path)
+        os.mkdir(path)
 
 
 # Clears directory
@@ -89,11 +81,6 @@ def remove_dir(path):
     if os.path.isdir(path):
         shutil.rmtree(path)
 
-# Makes directory 
-def make_dir(path):
-    if not os.path.isdir(path):
-        print("Making dir.... " + path)
-        os.mkdir(path)
     
 
 if __name__ == "__main__":
