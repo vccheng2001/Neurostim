3700e7d7db145686a9eb890c25a9877aad2d7bf8
vccheng2001 <vccheng@andrew.cmu.edu>
2021-05-30 03:30:37 -0700

    adjust to make it work for normalized file

--- flatline_detection.py
+++ flatline_detection.py
@@ -20,27 +20,36 @@ DATA_DIR = os.path.join(ROOT_DIR, "data")
 dataset = "mit"
 excerpt = 37
 sample_rate = 10
+SCALE_FACTOR = 100
 FLATLINE_THRESHOLD = 0.01
 WINDOW_SIZE = 100
 
 
 def main():
+    # unnormalized file
     unnorm_file = f"{DATA_DIR}/{dataset}/preprocessing/excerpt{excerpt}/filtered_{sample_rate}hz.txt" 
-    # norm_file = f"{DATA_DIR}/{dataset}/preprocessing/excerpt{excerpt}/filtered_{sample_rate}hz_linear_100.norm"
     unnorm_flatline_start_end_times, unnorm_flatline_value = get_flatline_value(unnorm_file)
-    # norm_flatline_start_end_times, norm_flatline_value = get_flatline_value(norm_file)
+    
+    # normalized file
+    norm_file = f"{DATA_DIR}/{dataset}/preprocessing/excerpt{excerpt}/filtered_{sample_rate}hz_linear_100.norm"
+    norm_flatline_start_end_times, norm_flatline_value = get_flatline_value(norm_file)
 
 def get_flatline_value(file):
     # read file
     df = pd.read_csv(file, delimiter=',')
 
+
     # comment out 
     df = df.iloc[:10000]
+
+    # original plot
+    # df.plot(x ='Time', y='Value', kind = 'line')
+    # plt.show()
 
     # difference of values 1 sec apart 
     df['Diff'] = df['Value'].diff(10)
     # set to 0 if < THRESHOLD, else 1
-    df['Diff'] = np.where(abs(df['Diff']) >= FLATLINE_THRESHOLD, 1, 0)
+    df['Diff'] = np.where(abs(df['Diff']) >= FLATLINE_THRESHOLD * SCALE_FACTOR, 1, 0)
     # convert to binary string representation
     bin_list = df['Diff'].tolist()
     bin_str = ''.join(str(int(x)) for x in bin_list)
